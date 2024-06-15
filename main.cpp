f230bb76097aa5b57e303dad16dbfd95d88827ce
siddharth sondhi <siddharth.sondhi@gmail.com>
2024-06-14 22:40:50 -0700

    added camera, viewport, and color pixels using ray

--- main.cpp
+++ main.cpp
@@ -1,27 +1,66 @@
 #include <iostream>
+#include <fstream>
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    // linear interpolation(lerp), blendedValue=(1−a)⋅startValue+a⋅endValue
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0, 0.7, 0);
+}
+
 
 int main() {
-
     // Image
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
 
-    int image_width = 256;
-    int image_height = 256;
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    auto camera_center = point3(0, 0, 0);
+    
+    // Viewport widths less than one are ok since they are real valued.
+    auto viewport_height = 2.0;
+    // Use image_width/image_height instead of aspect ratio bc aspect ratio is the ideal ratio, whereas
+    // image_width/image_height is the actual ratio(may not be ideal bc of integers)
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    
 
     // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::ofstream out_file("image.ppm");
+    out_file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color = ray_color(r);
+            write_color(out_file, pixel_color);
         }
+
     }
+
+    std::clog << "\rDone.                 \n";
 }
+
